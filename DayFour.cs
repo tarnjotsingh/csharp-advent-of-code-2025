392792196b1e46e331e8d23590602cd2a088cb74
Tarnjot Singh Virdee <tarnjot.virdee@itv.com>
2025-12-10 10:38:04 +0000

    Working day 4 part 2

--- DayFour.cs
+++ DayFour.cs
@@ -15,48 +15,62 @@ public class DayFour() {
     // Lookup current row
     // Lookup below row, not if at bottom of row
     int CalcAccessibleRolls(List<string> rollStack) {
-        int pickablePaperRolls = 0;
+        int pickablePaperRolls, removedPaperRolls = 0;
+        List<string> updatedPaperStack, currentPaperStack = rollStack;
 
-        // Traverse down the 2d array
-        for(int i = 0; i < rollStack.Count; i++){
-            string crossIndicatedString = "";
-            int pickableThisRow = 0, strLen = rollStack[0].Length;
+        do {
+            pickablePaperRolls = 0;
+            updatedPaperStack = [];
 
-            // Traverse the tmpStack left to right.
-            for(int j = 0; j < strLen; j++) {
-                int adjacentRolls = 0;
-                char currentChar = rollStack[i][j];
+            // Traverse down the 2d array
+            for(int i = 0; i < currentPaperStack.Count; i++){
+                string crossIndicatedString = "";
+                int pickableThisRow = 0, strLen = currentPaperStack[0].Length;
 
-                // While traversing left to right, if we land on a paper roll @, then we check around it.
-                if(currentChar == '@') {
-                    List<int> rowPos = [i - 1, i, i + 1];
-                    List<int> colPos = [j - 1, j, j + 1];
+                // Traverse the tmpStack left to right.
+                for(int j = 0; j < strLen; j++) {
+                    int adjacentRolls = 0;
+                    char currentChar = currentPaperStack[i][j];
 
-                    // If current pos is a paper roll, then check above and below and count adjacent vals.
-                    // Loop to check row above and below.
-                    foreach(int r in rowPos) {
-                        if(r >= 0 && r < rollStack.Count) {
-                            foreach(int c in colPos) {
-                                if(c >= 0 && c < strLen)
-                                    adjacentRolls += rollStack[r][c] == '@' ? 1 : 0;
+                    // While traversing left to right, if we land on a paper roll @, then we check around it.
+                    if(currentChar == '@') {
+                        List<int> rowPos = [i - 1, i, i + 1];
+                        List<int> colPos = [j - 1, j, j + 1];
+
+                        // If current pos is a paper roll, then check above and below and count adjacent vals.
+                        // Loop to check row above and below.
+                        foreach(int r in rowPos) {
+                            if(r >= 0 && r < currentPaperStack.Count) {
+                                foreach(int c in colPos) {
+                                    if(c >= 0 && c < strLen)
+                                        adjacentRolls += currentPaperStack[r][c] == '@' ? 1 : 0;
+                                }
                             }
                         }
+
+                        adjacentRolls--;
+                        bool pickable = adjacentRolls < 4;
+                        pickableThisRow += pickable ? 1 : 0;
+                        crossIndicatedString += pickable ? '.' : '@';
+                    } else {
+                        crossIndicatedString += '.';
                     }
-                    adjacentRolls--;
-                    pickableThisRow += adjacentRolls < 4 ? 1 : 0;
-                    crossIndicatedString += adjacentRolls < 4 ? 'x' : '@';
-                } else{
-                    crossIndicatedString += '.';
                 }
+
+                Console.Write(crossIndicatedString + " ");
+                Console.WriteLine(pickableThisRow);
+
+                pickablePaperRolls += pickableThisRow;
+                updatedPaperStack.Add(crossIndicatedString);
+
             }
+            currentPaperStack = updatedPaperStack;
+            removedPaperRolls += pickablePaperRolls;
+        } while(pickablePaperRolls != 0);
 
-            Console.Write(crossIndicatedString + " ");
-            Console.WriteLine(pickableThisRow);
-            pickablePaperRolls += pickableThisRow;
-        }
-
-        return pickablePaperRolls;
+        return removedPaperRolls;
     }
+
 
     // A forklift can only pick up ONE roll of paper at a time.
     // You have to work through the 8 immediate positions around a rolls of paper.
