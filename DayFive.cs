13f3332e96fe699a546bf3d28519642d51b87a66
Tarnjot Singh Virdee <tarnjot.virdee@itv.com>
2025-12-14 02:33:13 +0000

    Partial solution

--- DayFive.cs
+++ DayFive.cs
@@ -13,18 +13,38 @@ public class DayFive() {
         return linesArray;
     }
 
-    bool testInRanges(string testVal, string[] testRanges) {
+    bool TestInRange(string testVal, string testRange) {
         long toCheck = Int64.Parse(testVal);
 
-        foreach(string testRange in testRanges) {
-            string[] split = testRange.Split('-');
-            long lower = Int64.Parse(split[0]);
-            long higher = Int64.Parse(split[1]);
+        string[] split = testRange.Split('-');
+        long lower = Int64.Parse(split[0]);
+        long higher = Int64.Parse(split[1]);
 
-            if(toCheck >= lower && toCheck <= higher)
-                return true;
+        return toCheck >= lower && toCheck <= higher;
+    }
+
+    int CountIDs(string[] testRanges) {
+        List<string> newRanges = [];
+        foreach(string testRange in testRanges) {
+            // Scan over all ranges to see if there's an overlap.
+            foreach(string scan in testRanges) {
+                string[] scanSplit = scan.Split('-');
+                long scanLower = Int64.Parse(scanSplit[0]);
+                long scanHigher = Int64.Parse(scanSplit[1]);
+
+                bool inRange = TestInRange(scanLower.ToString(), testRange);
+                if(!scan.Equals(testRange) && inRange) {
+                    Console.Write(scan + " ");
+                    Console.WriteLine(testRange);
+                }
+                else {
+                    newRanges.Add(testRange);
+                }
+            }
+
         }
-        return false;
+
+        return 0;
     }
 
 
@@ -36,7 +56,7 @@ public class DayFive() {
     // Idea is that while processing an ingredient ID, if it falls within one of the ranges
     // the ingredient is considered to be FRESH, otherwise SPOILED.
     public void Run() {
-        string[] inputs = ReadInput("inputs/dayfive/inputs.txt");
+        string[] inputs = ReadInput("inputs/dayfive/testinputs.txt");
 
         // Use regex to cleanly filter out the values from the input.
         var rangesRegex = new Regex(@"^(\d+)-(\d+)$");
@@ -44,13 +64,8 @@ public class DayFive() {
         string[] ranges = inputs.Where(s => rangesRegex.IsMatch(s)).ToArray();
         string[] values = inputs.Where(s => valueRegex.IsMatch(s)).ToArray();
 
-        int count = 0;
-        foreach(string v in values) {
-            bool result = testInRanges(v, ranges);
-            count += result ? 1 : 0;
-        }
-
-        Console.WriteLine(count);
+        int result = CountIDs(ranges);
+        // Console.Write(result);
     }
 
     static void Main() {
